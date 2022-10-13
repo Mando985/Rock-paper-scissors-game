5135044df3e9f4093ac146597bde3442c85fef23
Mando985 <115579677+Mando985@users.noreply.github.com>
2022-10-13 16:54:33 +0400

    Add files via upload

---
+++ stuff.py
@@ -0,0 +1,40 @@
+def game():
+    import random
+    l1 = ["rock", "paper", "scissors"]
+    a, b, c = l1
+    y = random.choice(l1)
+    str1 = input("Choose any one of the given option (type the option only):\nA) Rock\nB) Paper\nC) Scissors\n==> ")
+    x = str1.lower()
+    if x == "a":
+        if y == a:
+            print("you both chose rock, try again")
+            game()
+        elif y == b:
+            print("you lose, the computer chose paper")
+        elif y == c:
+            print("you win! the computer unfortunately chose scissors")
+    elif x == "b":
+        if y == a:
+            print("you win! the computer unfortunately chose rock")
+        elif y == b:
+            print("you both chose paper, try again")
+            game()
+        elif y == c:
+            print("you lose, the computer chose scissors")
+    elif x == "c":
+        if y == a:
+            print("you lose, the computer chose rock")
+        elif y == b:
+            print("you win! the computer unfortunately chose paper")
+        elif y == c:
+            print("you both chose scissors, try again")
+            game()
+    h=input("Do you want to play again? (Y/N)\n==>")
+    j=h.lower()
+    if j== "y":
+        game()
+    else:
+        pass
+
+game()
+
