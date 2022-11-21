e03496fcde29275c41a635ac36de386407e3e80f
Mando985 <115579677+Mando985@users.noreply.github.com>
2022-11-21 20:11:38 +0400

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,43 +1,54 @@
+def playagain():
+    h = input("Do you want to play again? (Y/N)\n==>")
+    j = h.lower()
+    if j == "y":
+        game()
+    elif j == "n":
+        pass
+
+
 def game():
     import random
-    l1 = ["rock", "paper", "scissors"]
+    l1 = ("rock", "paper", "scissors")
     a, b, c = l1
     y = random.choice(l1)
     str1 = input("Choose any one of the given option (type the option only):\nA) Rock\nB) Paper\nC) Scissors\n==> ")
     x = str1.lower()
-    options = ["a", "b", "c"]
+    options = ("a", "b", "c")
     if x in options:
         if x == "a":
-         if y == a:
-            print("you both chose rock, try again")
-            game()
-         elif y == b:
-            print("you lose, the computer chose paper")
-         elif y == c:
-            print("you win! the computer unfortunately chose scissors")
+            if y == a:
+                print("you both chose rock, try again")
+                game()
+            elif y == b:
+                print("you lose, the computer chose paper")
+                playagain()
+            elif y == c:
+                print("you win! the computer unfortunately chose scissors")
+                playagain()
         elif x == "b":
-           if y == a:
-            print("you win! the computer unfortunately chose rock")
-           elif y == b:
-            print("you both chose paper, try again")
-            game()
-           elif y == c:
-            print("you lose, the computer chose scissors")
+            if y == a:
+                print("you win! the computer unfortunately chose rock")
+                playagain()
+            elif y == b:
+                print("you both chose paper, try again")
+                game()
+            elif y == c:
+                print("you lose, the computer chose scissors")
+                playagain()
         elif x == "c":
-           if y == a:
-            print("you lose, the computer chose rock")
-           elif y == b:
-            print("you win! the computer unfortunately chose paper")
-           elif y == c:
-            print("you both chose scissors, try again")
-            game()
-        h = input("Do you want to play again? (Y/N)\n==>")
-        j = h.lower()
-        if j == "y":
-           game()    
+            if y == a:
+                print("you lose, the computer chose rock")
+                playagain()
+            elif y == b:
+                print("you win! the computer unfortunately chose paper")
+                playagain()
+            elif y == c:
+                print("you both chose scissors, try again")
+                game()
     else:
         print("Invalid input, try again")
-        game()       
+        game()
 
 
-game()     
+game()
